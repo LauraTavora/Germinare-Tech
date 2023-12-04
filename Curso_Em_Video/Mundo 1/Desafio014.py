1dfc5706c8b5a25b20a8a80c18ed763d8ba0b9bd
LauraTavora <laurafranciscotavora5@gmail.com>
2023-12-04 17:42:02 -0300

    deleted:    Curso_Em_Video/Mundo 1/Desafio001.py 	deleted:    Curso_Em_Video/Mundo 1/Desafio002.py 	deleted:    Curso_Em_Video/Mundo 1/Desafio003.py 	deleted:    Curso_Em_Video/Mundo 1/Desafio004.py 	deleted:    Curso_Em_Video/Mundo 1/Desafio005.py 	deleted:    Curso_Em_Video/Mundo 1/Desafio006.py 	deleted:    Curso_Em_Video/Mundo 1/Desafio007.py 	deleted:    Curso_Em_Video/Mundo 1/Desafio008.py 	deleted:    Curso_Em_Video/Mundo 1/Desafio009.py 	deleted:    Curso_Em_Video/Mundo 1/Desafio010.py 	deleted:    Curso_Em_Video/Mundo 1/Desafio011.py 	deleted:    Curso_Em_Video/Mundo 1/Desafio012.py 	deleted:    Curso_Em_Video/Mundo 1/Desafio013.py 	deleted:    Curso_Em_Video/Mundo 1/Desafio014.py 	deleted:    Semana11-12_desafios/teste.py

--- Curso_Em_Video/Mundo 1/Desafio014.py
+++
@@ -1,6 +0,0 @@
-# Escreva um programa que converta uma temperatura digitada em °C e converta para °F.
-
-temp = float(input('Informe a temperatura em °C: '))
-f = ((9 * temp) / 5) + 32
-
-print(f'A temperatura de {temp}°C corresponde a {f}°F!')
