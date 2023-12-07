6c90c50ebe9e35649f84dfb39b6af19c86423878
LauraTavora <laurafranciscotavora5@gmail.com>
2023-12-07 18:07:38 -0300

    deleted:    Bibliotecas/random/Desafio1.py 	deleted:    Bibliotecas/random/Desafio2.py 	deleted:    Bibliotecas/random/Desafio3.py 	deleted:    Bibliotecas/random/teoria.py

--- Bibliotecas/random/Desafio1.py
+++
@@ -1,9 +0,0 @@
-'''
-1. Você quer simular a opção de jogar uma moeda e resultar em cara ou coroa
-    - Jogue as opções dentro de uma variável e depois crie um programa que imprima 'cara' ou 'coroa' na tela.
-'''
-import random
-
-
-opções = ['cara', 'coroa']
-print(random.choice(opções))
